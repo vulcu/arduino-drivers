9c274e6ab19d470ff1d0fc9955dd97c578a0a525
Winry Litwa-Vulcu <winry.litwavulcu@qsc.com>
2024-04-03 14:52:56 -0700

    feat(PCAL6408): add `set` and `get`, add register writes to `init`

--- io/PCAL6408A.cpp
+++ io/PCAL6408A.cpp
@@ -7,58 +7,147 @@
 #include <Wire.h>
 #include "PCAL6408A.h"
 
-#ifndef PCAL6408A_REGISTER_BIT_FIELD_NAMES
+namespace NXP {
+  using namespace PCAL6408ATypes;
 
-  #define PCAL6408A_REGISTER_BIT_FIELD_NAMES
+  // PCAL6408A default register values for initialization
+  static const uint8_t PCAL6408A::default_config[12] PROGMEM = 
+  {
+    0x00,                 // Input Port
+    0xFF,                 // Output Port
+    0x00,                 // Polarity Inversion
+    0xFF,                 // Configuration
+    0xFF,                 // Output Drive Strength 0
+    0xFF,                 // Output Drive Strength 1
+    0x00,                 // Input Latch
+    0x00,                 // Pull-up/Pull-down Enable
+    0xFF,                 // Pull-up/Pull-down Selection
+    0xFF,                 // Interrupt Mask
+    0x00,                 // Interrupt Status
+    0x00                  // Output Port Configuration
+  };
 
-#endif
+  PCAL6408A::PCAL6408A(uint8_t i2c_address, uint8_t reset_n, uint8_t interrupt_n, TwoWire *pWire) : 
+                      i2c_address(i2c_address), reset_n(reset_n), interrupt_n(interrupt_n) {
+    this->pWire = pWire;
+    resetActiveConfig();
+  };
 
-// PCAL6408A default register values for initialization
-const int16_t default_config[12] PROGMEM = 
-{
-  0x00,                 //Test Register (must be 0x00)
-  0x00,                 //Test Register (must be 0x00)
-  0x00,                 //Test Register (must be 0x00)
-  0x00,                 //Test Register (must be 0x00)
-  0x00,                 //Test Register (must be 0x00)
-  0x00,                 //Test Register (must be 0x00)
-  0x00,                 //Test Register (must be 0x00)
-  0x00,                 //Test Register (must be 0x00)
-  0x00,                 //Test Register (must be 0x00)
-  0x00,                 //Test Register (must be 0x00)
-  0x00,                 //Test Register (must be 0x00)
-  0x00                  //Test Register (must be 0x00)
-};
+  void PCAL6408A::init(void) {
+    // set PCAL6408A RESET pin HIGH to put in reset
+    shutdown();
+    resetActiveConfig();
 
-PCAL6408A::PCAL6408A(uint8_t i2c_address, uint8_t reset_n, uint8_t interrupt_n, TwoWire *Wire) : 
-                     i2c_address(i2c_address), reset_n(reset_n), interrupt_n(interrupt_n) {
-  this->Wire = Wire;
-  resetActiveConfig();
-};
+    // configure output port
+    pWire->beginTransmission(i2c_address);
+      pWire->write(OUTPUT_PORT_PTR);
+      pWire->write(pgm_read_byte(&(default_config[OUTPUT_PORT])));
+    pWire->endTransmission();
 
+    // configure polarity inversion
+    pWire->beginTransmission(i2c_address);
+      pWire->write(POLARITY_INVERSION_PTR);
+      pWire->write(pgm_read_byte(&(default_config[POLARITY_INVERSION])));
+    pWire->endTransmission();
 
-void PCAL6408A::init(void) {
-  // set AK5558 PDN pin HIGH to put in reset
-  shutdown();
+    // configure pin I/O direction
+    pWire->beginTransmission(i2c_address);
+      pWire->write(CONFIGURATION_PTR);
+      pWire->write(pgm_read_byte(&(default_config[CONFIGURATION])));
+    pWire->endTransmission();
 
-  resetActiveConfig();
+    // configure output drive strength
+    pWire->beginTransmission(i2c_address);
+      pWire->write(DRIVE_STRENGTH_0_PTR);
+      pWire->write(pgm_read_byte(&(default_config[DRIVE_STRENGTH_0])));
+    pWire->endTransmission();
+    pWire->beginTransmission(i2c_address);
+      pWire->write(DRIVE_STRENGTH_1_PTR);
+      pWire->write(pgm_read_byte(&(default_config[DRIVE_STRENGTH_1])));
+    pWire->endTransmission();
 
-  // set AK5558 PDN pin Low to take out of reset
-  enable();
-}
+    // configure input port latching
+    pWire->beginTransmission(i2c_address);
+      pWire->write(INPUT_LATCH_PTR);
+      pWire->write(pgm_read_byte(&(default_config[INPUT_LATCH])));
+    pWire->endTransmission();
 
+    // configure pull-up/pull-down enable
+    pWire->beginTransmission(i2c_address);
+      pWire->write(PULLUP_PULLDOWN_EN_PTR);
+      pWire->write(pgm_read_byte(&(default_config[PULLUP_PULLDOWN_EN])));
+    pWire->endTransmission();
 
-void PCAL6408A::enable(void) {
-  pinMode(reset_n, OUTPUT);
-  digitalWrite(reset_n, LOW);
-}
+    // configure pull-up/pull-down selection
+    pWire->beginTransmission(i2c_address);
+      pWire->write(PULLUP_PULLDOWN_SEL_PTR);
+      pWire->write(pgm_read_byte(&(default_config[PULLUP_PULLDOWN_SEL])));
+    pWire->endTransmission();
 
+    // configure interrupt mask
+    pWire->beginTransmission(i2c_address);
+      pWire->write(INTERRUPT_MASK_PTR);
+      pWire->write(pgm_read_byte(&(default_config[INTERRUPT_MASK])));
+    pWire->endTransmission();
 
-void PCAL6408A::shutdown(void) {
-  pinMode(reset_n, OUTPUT);
-  digitalWrite(reset_n, HIGH);
-}
+    // configure input port
+    pWire->beginTransmission(i2c_address);
+      pWire->write(OUTPUT_PORT_CONFIG_PTR);
+      pWire->write(pgm_read_byte(&(default_config[OUTPUT_PORT_CONFIG])));
+    pWire->endTransmission();
 
-void PCAL6408A::resetActiveConfig(void) {
-  memcpy_P(active_config, default_config, sizeof(default_config));
+    // set PCAL6408A RESET pin Low to take out of reset
+    enable();
+  }
+
+  void PCAL6408A::enable(void) {
+    pinMode(reset_n, OUTPUT);
+    digitalWrite(reset_n, LOW);
+  }
+
+  bool PCAL6408A::get(register_bitmask_t input_port) {
+    // read logical state of specified port bit
+    pWire->beginTransmission(i2c_address);
+    pWire->write(INPUT_PORT);
+    pWire->endTransmission();
+    delayMicroseconds(50);
+    pWire->requestFrom(i2c_address, 1U);
+    delayMicroseconds(50);
+    uint8_t read_data = (uint8_t)pWire->read();
+    pWire->endTransmission();
+
+    return (bool)((read_data & input_port) >> input_port);
+  }
+
+  void PCAL6408A::set(register_bitmask_t output_port, bool value) {
+    // read logical state of specified port first to avoid overwriting data
+    pWire->beginTransmission(i2c_address);
+    pWire->write(OUTPUT_PORT);
+    pWire->endTransmission();
+    delayMicroseconds(50);
+    pWire->requestFrom(i2c_address, 1U);
+    delayMicroseconds(50);
+    uint8_t read_data = (uint8_t)pWire->read();
+    pWire->endTransmission();
+
+    // write logical state of specified port bit
+    pWire->beginTransmission(i2c_address);
+      pWire->write(OUTPUT_PORT);
+      if (value = HIGH) {
+        pWire->write((read_data | output_port));
+      }
+      else {
+        pWire->write((read_data & ~output_port));
+      }
+    pWire->endTransmission();
+  }
+
+  void PCAL6408A::shutdown(void) {
+    pinMode(reset_n, OUTPUT);
+    digitalWrite(reset_n, HIGH);
+  }
+
+  void PCAL6408A::resetActiveConfig(void) {
+    memcpy_P(active_config, default_config, sizeof(default_config));
+  }
 }
--- io/PCAL6408A.h
+++ io/PCAL6408A.h
@@ -6,83 +6,132 @@
 #ifndef PCAL6408A_H
 #define PCAL6408A_H
 
-  enum PCAL6408A_port_index {
-      PCAL6408A_PORT0 = 0,  // PCAS6408A Port 0
-      PCAL6408A_PORT1,      // PCAS6408A Port 1
-      PCAL6408A_PORT2,      // PCAS6408A Port 2
-      PCAL6408A_PORT3,      // PCAS6408A Port 3
-      PCAL6408A_PORT4,      // PCAS6408A Port 4
-      PCAL6408A_PORT5,      // PCAS6408A Port 5
-      PCAL6408A_PORT6,      // PCAS6408A Port 6
-      PCAL6408A_PORT7,      // PCAS6408A Port 7
-    };
-
-  class PCAL6408A {
-    public:
-      /*! @brief Class constructor
-       *
-       * @details A more elaborate description of the constructor.
-       * 
-       * @param i2c_addr The physical device's I2C address
-       * @param reset_n  The microcontroller pin connected to the device RESET_L next
-       * @param Wire     A pointer to an instance of the TwoWire class
-       */
-      PCAL6408A(const uint8_t i2c_addr, 
-                const uint8_t reset_n, 
-                const uint8_t interrupt_n, 
-                TwoWire* wire);
-
-      /*! @brief  Initialize the PCAL6408A
-       *
-       * @details Initialize the device and write default config values to all registers
-       * 
-       * @warning This will reset all device registers to the default configuration 
-       */
-      void init(void);
-
-      /*! @brief  Enable the PCAL6408A by taking it out of reset
-       *
-       * @details A more elaborate description of the class member.
-       */
-      void enable(void);
-
-      /*! @brief  Disable the PCAL6408A by placing it in reset
-       *
-       * @details A more elaborate description of the class member.
-       */
-      void shutdown(void);
-
-    private:
-      /*! @enum PCAL6408A register address index */
-      enum PCAL6408A_register_index {
-        PCAL6408A_INPUT_PORT = 0,       // Input Port Register (Read-Only)
-        PCAL6408A_OUTPUT_PORT,          // Output Port Register
-        PCAL6408A_POLARITY_INVERSION,   // Polarity Inversion (Input Only)
-        PCAL6408A_CONFIGURATION,        // Pin Direction (I/O) Configuration
-        PCAL6408A_DRIVE_STRENGTH_0,     // Output Drive Strength (Port 0-3)
-        PCAL6408A_DRIVE_STRENGTH_1,     // Output Drive Strength (Port 4-7)
-        PCAL6408A_INPUT_LATCH,          // Enable/Disable Input Pin Latching
-        PCAL6408A_PULLUP_PULLDOWN_EN,   // Enable/Disable Pin PU/PD Resistor
-        PCAL6408A_PULLUP_PULLDOWN_SEL,  // Configure Pin PU/PD Resistor Mode
-        PCAL6408A_INTERRUPT_MASK,       // Enable/Disable Interrupts Per-Pin
-        PCAL6408A_INTERRUPT_STATUS,     // Interrupt Status (Read-Only)
-        PCAL6408A_OUTPUT_PORT_CONFIG,   // Set Outputs as Push/Pull or OD
+  namespace NXP {
+    namespace PCAL6408ATypes {
+      /*! @enum PCAL6408A register bitmasks */
+      enum register_bitmask_t {
+        BIT0 = (0x01 << 0),  
+        BIT1 = (0x01 << 1), 
+        BIT2 = (0x01 << 2), 
+        BIT3 = (0x01 << 3), 
+        BIT4 = (0x01 << 4), 
+        BIT5 = (0x01 << 5), 
+        BIT6 = (0x01 << 6),  
+        BIT7 = (0x01 << 7),  
       };
 
-      const uint8_t i2c_address;
-      const uint8_t interrupt_n;
-      const uint8_t reset_n;
-      uint8_t active_config[12];
-      TwoWire *Wire;
+      /*! @enum PCAL6408A current control register bitmasks */
+      enum current_control_bitmask_t {
+        CC0 = (0x01 << 0),  
+        CC1 = (0x01 << 2), 
+        CC2 = (0x01 << 4), 
+        CC3 = (0x01 << 6),  
+      };
 
-      /*! @brief Reset the PCAL6408A configuration in memory
-       *
-       * @details Reset the device configuration in SRAM to the default configuration.
-       * 
-       * @warning This will reset the device configuration in memory. It will
-       *          not apply these changes to the physical device registers.
-       */
-      void resetActiveConfig(void);
-  };
+      /*! @enum PCAL6408A pointer register command bytes */
+      enum register_pointer_t {
+        INPUT_PORT_PTR          = 0x00, // Input Port Register (Read-Only)
+        OUTPUT_PORT_PTR         = 0x01, // Output Port Register
+        POLARITY_INVERSION_PTR  = 0x02, // Polarity Inversion (Input Only)
+        CONFIGURATION_PTR       = 0x03, // Pin Direction (I/O) Configuration
+        DRIVE_STRENGTH_0_PTR    = 0x40, // Output Drive Strength (Port 0-3)
+        DRIVE_STRENGTH_1_PTR    = 0x41, // Output Drive Strength (Port 4-7)
+        INPUT_LATCH_PTR         = 0x42, // Enable/Disable Input Pin Latching
+        PULLUP_PULLDOWN_EN_PTR  = 0x43, // Enable/Disable Pin PU/PD Resistor
+        PULLUP_PULLDOWN_SEL_PTR = 0x44, // Configure Pin PU/PD Resistor Mode
+        INTERRUPT_MASK_PTR      = 0x45, // Enable/Disable Interrupts Per-Pin
+        INTERRUPT_STATUS_PTR    = 0x46, // Interrupt Status (Read-Only)
+        OUTPUT_PORT_CONFIG_PTR  = 0x4F, // Set Outputs as Push/Pull or OD
+      };
+
+      /*! @enum PCAL6408A register name index */
+      enum register_name_t {
+        INPUT_PORT          = 0,  // Input Port Register (Read-Only)
+        OUTPUT_PORT,              // Output Port Register
+        POLARITY_INVERSION,       // Polarity Inversion (Input Only)
+        CONFIGURATION,            // Pin Direction (I/O) Configuration
+        DRIVE_STRENGTH_0,         // Output Drive Strength (Port 0-3)
+        DRIVE_STRENGTH_1,         // Output Drive Strength (Port 4-7)
+        INPUT_LATCH,              // Enable/Disable Input Pin Latching
+        PULLUP_PULLDOWN_EN,       // Enable/Disable Pin PU/PD Resistor
+        PULLUP_PULLDOWN_SEL,      // Configure Pin PU/PD Resistor Mode
+        INTERRUPT_MASK,           // Enable/Disable Interrupts Per-Pin
+        INTERRUPT_STATUS,         // Interrupt Status (Read-Only)
+        OUTPUT_PORT_CONFIG,       // Set Outputs as Push/Pull or OD
+      };
+    }
+
+    class PCAL6408A {
+      public:
+        /*! @brief Class constructor
+        *
+        * @details A more elaborate description of the constructor.
+        * 
+        * @param i2c_addr The physical device's I2C address
+        * @param reset_n  The microcontroller pin connected to the device RESET_L next
+        * @param pWire     A pointer to an instance of the TwoWire class
+        */
+        PCAL6408A(const uint8_t i2c_addr, 
+                  const uint8_t reset_n, 
+                  const uint8_t interrupt_n, 
+                  TwoWire* pWire);
+
+        /*! @brief  Initialize the PCAL6408A
+        *
+        * @details Initialize the device and write default config values to all registers
+        * 
+        * @warning This will reset all device registers to the default configuration 
+        */
+        void init(void);
+
+        /*! @brief  Enable the PCAL6408A by taking it out of reset
+        *
+        * @details A more elaborate description of the class member.
+        */
+        void enable(void);
+
+        /*! @brief Class constructor
+        *
+        * @details A more elaborate description of the constructor.
+        * 
+        * @param input_port The input port register bit to read
+        * 
+        * @returns 
+        */
+        bool get(PCAL6408ATypes::register_bitmask_t input_port);
+
+        /*! @brief Class constructor
+        *
+        * @details A more elaborate description of the constructor.
+        * 
+        * @param output_port The output port register bit to write
+        * @param value       The boolean value to write (0 or 1)
+        */
+        void set(PCAL6408ATypes::register_bitmask_t output_port, bool value);
+
+        /*! @brief  Disable the PCAL6408A by placing it in reset
+        *
+        * @details A more elaborate description of the class member.
+        */
+        void shutdown(void);
+
+      private:
+        const uint8_t i2c_address;
+        const uint8_t interrupt_n;
+        const uint8_t reset_n;
+        uint8_t active_config[12];
+        static const uint8_t default_config[12] PROGMEM;
+        TwoWire *pWire;
+
+        /*! @brief Reset the PCAL6408A configuration in memory
+        *
+        * @details Reset the device configuration in SRAM to the default configuration.
+        * 
+        * @warning This will reset the device configuration in memory. It will
+        *          not apply these changes to the physical device registers.
+        */
+        void resetActiveConfig(void);
+    };
+  }
 
 #endif
