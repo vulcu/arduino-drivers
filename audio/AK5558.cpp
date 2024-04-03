2210cfc7eeb605648007dabfc065cdca91887a21
vulcu <19890187+vulcu@users.noreply.github.com>
2024-04-02 23:02:25 -0700

    feat: WIP put AK5558 class in AKM namespace

--- audio/AK5558.cpp
+++ audio/AK5558.cpp
@@ -46,123 +46,118 @@
 
 #endif
 
-// AK5558 default register values for initialization
-static const uint8_t default_config[8] PROGMEM = 
-{
-  0xFF,                 //Power Management
-  0x01,                 //Channel Summing and Timing Reset
-  0x01,                 //Clocking, DAI Mode, HP Filter
-  0x00,                 //TDM Mode Selection
-  0x00,                 //LP Filter, DSD Mode
-  0x00,                 //DSD Configuration
-  0x00,                 //Test Register (must be 0x00)
-  0x00                  //Test Register (must be 0x00)
-};
+namespace AKM {
+  // AK5558 default register values for initialization
+  static const uint8_t default_config[8] PROGMEM = 
+  {
+    0xFF,                 //Power Management
+    0x01,                 //Channel Summing and Timing Reset
+    0x01,                 //Clocking, DAI Mode, HP Filter
+    0x00,                 //TDM Mode Selection
+    0x00,                 //LP Filter, DSD Mode
+    0x00,                 //DSD Configuration
+    0x00,                 //Test Register (must be 0x00)
+    0x00                  //Test Register (must be 0x00)
+  };
 
 
-AK5558::AK5558(uint8_t i2c_address, uint8_t reset_n, TwoWire *Wire) :
-               invert_mute(false), i2c_address(i2c_address), reset_n(reset_n) {
-  this->Wire = Wire;
-  resetActiveConfig();
-}
-
-
-void AK5558::init(void) {
-  // set AK5558 PDN pin HIGH to put in reset
-  shutdown();
-
-  // configure power management
-  Wire->beginTransmission(i2c_address);
-    Wire->write(AK5558_PWRMGMT1);
-    Wire->write(pgm_read_byte(&(default_config[AK5558_PWRMGMT1])));
-  Wire->endTransmission();
-
-  // configure channel summing and timing reset
-  Wire->beginTransmission(i2c_address);
-    Wire->write(AK5558_PWRMGMT2);
-    Wire->write(pgm_read_byte(&(default_config[AK5558_PWRMGMT2])));
-  Wire->endTransmission();
-
-  // configure clocking, DAI mode, HP filter
-  Wire->beginTransmission(i2c_address);
-    Wire->write(AK5558_CONTROL1);
-    Wire->write(pgm_read_byte(&(default_config[AK5558_CONTROL1])));
-  Wire->endTransmission();
-
-  // configure TDM mode selection
-  Wire->beginTransmission(i2c_address);
-    Wire->write(AK5558_CONTROL2);
-    Wire->write(pgm_read_byte(&(default_config[AK5558_CONTROL2])));
-  Wire->endTransmission();
-
-  // configure LP filter and DSD mode
-  Wire->beginTransmission(i2c_address);
-    Wire->write(AK5558_CONTROL3);
-    Wire->write(pgm_read_byte(&(default_config[AK5558_CONTROL3])));
-  Wire->endTransmission();
-
-  //  configure DSD frequency, phase, clocking
-  Wire->beginTransmission(i2c_address);
-    Wire->write(AK5558_DSD);
-    Wire->write(pgm_read_byte(&(default_config[AK5558_DSD])));
-  Wire->endTransmission();
-
-  //  configure Test registers (MUST be 0x00)
-  Wire->beginTransmission(i2c_address);
-    Wire->write(AK5558_TEST1);
-    Wire->write(pgm_read_byte(&(default_config[AK5558_TEST1])));
-  Wire->endTransmission();
-  Wire->beginTransmission(i2c_address);
-    Wire->write(AK5558_TEST2);
-    Wire->write(pgm_read_byte(&(default_config[AK5558_TEST2])));
-  Wire->endTransmission();
-
-  resetActiveConfig();
-
-  // set AK5558 PDN pin Low to take out of reset
-  enable();
-}
-
-
-void AK5558::enable(void) {
-  pinMode(reset_n, OUTPUT);
-  digitalWrite(reset_n, LOW);
-}
-
-
-void AK5558::shutdown(void) {
-  pinMode(reset_n, OUTPUT);
-  digitalWrite(reset_n, HIGH);
-}
-
-
-void AK5558::invertMuteLogic(const bool invert_mute) {
-  this->invert_mute = invert_mute;
-}
-
-
-void AK5558::mute(void) {
-  // if the mute signal is inverted then clear control bit to mute
-  if (invert_mute) {
-    // TODO: mute device by setting bit LOW
+  AK5558::AK5558(uint8_t i2c_address, uint8_t reset_n, TwoWire *Wire) :
+                invert_mute(false), i2c_address(i2c_address), reset_n(reset_n) {
+    this->Wire = Wire;
+    resetActiveConfig();
   }
-  else {
-    // TODO: mute device by setting bit HIGH
+
+  void AK5558::init(void) {
+    // set AK5558 PDN pin HIGH to put in reset
+    shutdown();
+
+    // configure power management
+    Wire->beginTransmission(i2c_address);
+      Wire->write(AK5558_PWRMGMT1);
+      Wire->write(pgm_read_byte(&(default_config[AK5558_PWRMGMT1])));
+    Wire->endTransmission();
+
+    // configure channel summing and timing reset
+    Wire->beginTransmission(i2c_address);
+      Wire->write(AK5558_PWRMGMT2);
+      Wire->write(pgm_read_byte(&(default_config[AK5558_PWRMGMT2])));
+    Wire->endTransmission();
+
+    // configure clocking, DAI mode, HP filter
+    Wire->beginTransmission(i2c_address);
+      Wire->write(AK5558_CONTROL1);
+      Wire->write(pgm_read_byte(&(default_config[AK5558_CONTROL1])));
+    Wire->endTransmission();
+
+    // configure TDM mode selection
+    Wire->beginTransmission(i2c_address);
+      Wire->write(AK5558_CONTROL2);
+      Wire->write(pgm_read_byte(&(default_config[AK5558_CONTROL2])));
+    Wire->endTransmission();
+
+    // configure LP filter and DSD mode
+    Wire->beginTransmission(i2c_address);
+      Wire->write(AK5558_CONTROL3);
+      Wire->write(pgm_read_byte(&(default_config[AK5558_CONTROL3])));
+    Wire->endTransmission();
+
+    //  configure DSD frequency, phase, clocking
+    Wire->beginTransmission(i2c_address);
+      Wire->write(AK5558_DSD);
+      Wire->write(pgm_read_byte(&(default_config[AK5558_DSD])));
+    Wire->endTransmission();
+
+    //  configure Test registers (MUST be 0x00)
+    Wire->beginTransmission(i2c_address);
+      Wire->write(AK5558_TEST1);
+      Wire->write(pgm_read_byte(&(default_config[AK5558_TEST1])));
+    Wire->endTransmission();
+    Wire->beginTransmission(i2c_address);
+      Wire->write(AK5558_TEST2);
+      Wire->write(pgm_read_byte(&(default_config[AK5558_TEST2])));
+    Wire->endTransmission();
+
+    resetActiveConfig();
+
+    // set AK5558 PDN pin Low to take out of reset
+    enable();
   }
-}
 
-
-void AK5558::unmute(void) {
-  // if the mute signal is inverted then set control bit to unmute
-  if (invert_mute) {
-    // TODO: unmute device by setting bit HIGH
+  void AK5558::enable(void) {
+    pinMode(reset_n, OUTPUT);
+    digitalWrite(reset_n, LOW);
   }
-  else {
-    // TODO: unmute device by setting bit LOW
+
+  void AK5558::shutdown(void) {
+    pinMode(reset_n, OUTPUT);
+    digitalWrite(reset_n, HIGH);
   }
-}
 
+  void AK5558::invertMuteLogic(const bool invert_mute) {
+    this->invert_mute = invert_mute;
+  }
 
-void AK5558::resetActiveConfig(void) {
-  memcpy_P(active_config, default_config, sizeof(default_config));
+  void AK5558::mute(void) {
+    // if the mute signal is inverted then clear control bit to mute
+    if (invert_mute) {
+      // TODO: mute device by setting bit LOW
+    }
+    else {
+      // TODO: mute device by setting bit HIGH
+    }
+  }
+
+  void AK5558::unmute(void) {
+    // if the mute signal is inverted then set control bit to unmute
+    if (invert_mute) {
+      // TODO: unmute device by setting bit HIGH
+    }
+    else {
+      // TODO: unmute device by setting bit LOW
+    }
+  }
+
+  void AK5558::resetActiveConfig(void) {
+    memcpy_P(active_config, default_config, sizeof(default_config));
+  }
 }
--- audio/AK5558.h
+++ audio/AK5558.h
@@ -6,87 +6,124 @@
 #ifndef AK5558_H
 #define AK5558_H
 
-  /*! @brief AKM AK5558 Analog-to-Digital Converter driver for Arduino
-   *
-   * @details A more elaborate description of the class.
-   */
-  class AK5558 {
-    public:
-      /*! @brief Class constructor
-       *
-       * @details A more elaborate description of the constructor.
-       * 
-       * @param i2c_addr The physical device's I2C address
-       * @param reset_n  The microcontroller pin connected to the device RESET_L next
-       * @param Wire     A pointer to an instance of the TwoWire class
-       */
-      AK5558(const uint8_t i2c_addr, const uint8_t reset_n, TwoWire *Wire);
+  namespace AKM {
+    namespace AK5558Types {
+      // #define PW1_BM     (0x01 << 0)
+      // #define PW2_BM     (0x01 << 1)
+      // #define PW3_BM     (0x01 << 2)
+      // #define PW4_BM     (0x01 << 3)
+      // #define PW5_BM     (0x01 << 4)
+      // #define PW6_BM     (0x01 << 5)
+      // #define PW7_BM     (0x01 << 6)
+      // #define PW8_BM     (0x01 << 7)
 
-      /*! @brief  Initialize the AK5558
-       *
-       * @details Initialize the device and write default config values to all registers
-       * 
-       * @warning This will reset all device registers to the default configuration 
-       */
-      void init(void);
+      // #define RSTN_BM    (0x01 << 0)
+      // #define MONO1_BM   (0x01 << 1)
+      // #define MONO2_BM   (0x01 << 2)
 
-      /*! @brief  Enable the AK5558 by taking it out of reset
-       *
-       * @details A more elaborate description of the class member.
-       */
-      void enable(void);
+      // #define HFPE_BM    (0x01 << 0)
+      // #define DIF0_BM    (0x01 << 1)
+      // #define DIF1_BM    (0x01 << 2)
+      // #define CKS0_BM    (0x01 << 3)
+      // #define CKS1_BM    (0x01 << 4)
+      // #define CKS2_BM    (0x01 << 5)
+      // #define CKS3_BM    (0x01 << 6)
 
-      /*! @brief  Disable the AK5558 by placing it in reset
-       *
-       * @details A more elaborate description of the class member.
-       */
-      void shutdown(void);
+      // #define TDM0_BM    (0x01 << 5)
+      // #define TDM1_BM    (0x01 << 6)
 
-      /*! @brief  Invert mute signal logic
-       *
-       * @details A more elaborate description of the class member.
-       */
-      void invertMuteLogic(const bool invert_mute);
+      // #define SLOW_BM    (0x01 << 0)
+      // #define SD_BM      (0x01 << 1)
 
-      /*! @brief  Unmute the AK5558
-       *
-       * @details A more elaborate description of the class member.
-       */
-      void mute(void);
-      
-      /*! @brief  Unmute the AK5558
-       *
-       * @details A more elaborate description of the class member.
-       */
-      void unmute(void);
+      // #define DSDSEL0_BM (0x01 << 0)
+      // #define DSDSEL1_BM (0x01 << 1)
+      // #define DCKB_BM    (0x01 << 2)
+      // #define PMOD_BM    (0x01 << 3)
+      // #define DCKS_BM    (0x01 << 5)
+    }
 
-    private:
-      /*! @enum AK5558 register address index */
-      enum AK5558_register_index {
-        AK5558_PWRMGMT1 = 0,  //Power Management
-        AK5558_PWRMGMT2,      //Channel Summing and Timing Reset
-        AK5558_CONTROL1,      //Clocking, DAI Mode, HP Filter
-        AK5558_CONTROL2,      //TDM Mode Selection
-        AK5558_CONTROL3,      //LP Filter, DSD Mode
-        AK5558_DSD,           //DSD Configuration
-        AK5558_TEST1,         //Test Register (must be 0x00)
-        AK5558_TEST2,         //Test Register (must be 0x00)
-      };
+    /*! @brief AKM AK5558 Analog-to-Digital Converter driver for Arduino
+    *
+    * @details A more elaborate description of the class.
+    */
+    class AK5558 {
+      public:
+        /*! @brief Class constructor
+        *
+        * @details A more elaborate description of the constructor.
+        * 
+        * @param i2c_addr The physical device's I2C address
+        * @param reset_n  The microcontroller pin connected to the device RESET_L next
+        * @param Wire     A pointer to an instance of the TwoWire class
+        */
+        AK5558(const uint8_t i2c_addr, const uint8_t reset_n, TwoWire *Wire);
 
-      const uint8_t i2c_address;
-      const uint8_t reset_n;
-      bool invert_mute;
-      uint8_t active_config[8];
-      TwoWire *Wire;
+        /*! @brief  Initialize the AK5558
+        *
+        * @details Initialize the device and write default config values to all registers
+        * 
+        * @warning This will reset all device registers to the default configuration 
+        */
+        void init(void);
 
-      /*! @brief Reset the AK5558 configuration in memory
-       *
-       * @details Reset the device configuration in SRAM to the default configuration.
-       * 
-       * @warning This will reset the device configuration in memory. It will
-       *          not apply these changes to the physical device registers.
-       */
-      void resetActiveConfig(void);
-  };
+        /*! @brief  Enable the AK5558 by taking it out of reset
+        *
+        * @details A more elaborate description of the class member.
+        */
+        void enable(void);
+
+        /*! @brief  Disable the AK5558 by placing it in reset
+        *
+        * @details A more elaborate description of the class member.
+        */
+        void shutdown(void);
+
+        /*! @brief  Invert mute signal logic
+        *
+        * @details A more elaborate description of the class member.
+        */
+        void invertMuteLogic(const bool invert_mute);
+
+        /*! @brief  Unmute the AK5558
+        *
+        * @details A more elaborate description of the class member.
+        */
+        void mute(void);
+        
+        /*! @brief  Unmute the AK5558
+        *
+        * @details A more elaborate description of the class member.
+        */
+        void unmute(void);
+
+      private:
+        /*! @enum AK5558 register address index */
+        enum AK5558_register_index {
+          AK5558_PWRMGMT1 = 0,  //Power Management
+          AK5558_PWRMGMT2,      //Channel Summing and Timing Reset
+          AK5558_CONTROL1,      //Clocking, DAI Mode, HP Filter
+          AK5558_CONTROL2,      //TDM Mode Selection
+          AK5558_CONTROL3,      //LP Filter, DSD Mode
+          AK5558_DSD,           //DSD Configuration
+          AK5558_TEST1,         //Test Register (must be 0x00)
+          AK5558_TEST2,         //Test Register (must be 0x00)
+        };
+
+        const uint8_t i2c_address;
+        const uint8_t reset_n;
+        bool invert_mute;
+        uint8_t active_config[8];
+        TwoWire *Wire;
+
+        /*! @brief Reset the AK5558 configuration in memory
+        *
+        * @details Reset the device configuration in SRAM to the default configuration.
+        * 
+        * @warning This will reset the device configuration in memory. It will
+        *          not apply these changes to the physical device registers.
+        */
+        void resetActiveConfig(void);
+    };
+  }
 
 #endif
